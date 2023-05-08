c2f6ecc1c22077b4aa5ad2b9f476e6604231cbe2
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2023-05-08 13:46:16 -0400

    start env stuff

--- src/main/java/org/openjfx/Main.java
+++ src/main/java/org/openjfx/Main.java
@@ -1,5 +1,13 @@
 package org.openjfx;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,6 +17,18 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
+        ArrayList<String> constants = new ArrayList<>();
+
+        try {
+            constants = (ArrayList<String>) Files.readAllLines(Path.of(".env"));
+        } catch (FileNotFoundException e) {
+            System.out.println("brr");
+        } catch (IOException e) {
+            System.out.println("Eeeee");
+        }
+
+        // TODO make this actually work
+
         var label = new Label("a");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
