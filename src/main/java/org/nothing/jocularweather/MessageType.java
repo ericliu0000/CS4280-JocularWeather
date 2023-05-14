8315ac2c121f2a9085ce52b21c8a37288a11f3da
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2023-05-13 22:53:57 -0400

    remove cfg files

---
+++ src/main/java/org/nothing/jocularweather/MessageType.java
@@ -0,0 +1,19 @@
+package org.nothing.jocularweather;
+
+/**
+ * Handles types of messages in program console output.
+ */
+enum MessageType {
+    /**
+     * Informational output; corresponds to green text.
+     */
+    JW_INFO,
+    /**
+     * Warning output: corresponds to amber text.
+     */
+    JW_WARN,
+    /**
+     * Output to indicate program error: corresponds to red text.
+     */
+    JW_ERROR
+}
